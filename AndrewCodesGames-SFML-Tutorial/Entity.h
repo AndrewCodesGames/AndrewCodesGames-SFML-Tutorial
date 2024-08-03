541f98b94156fd5517d3682cbbd5d88d2ef66514
Andrew Robinson <andrewrobinsonunited@gmail.com>
2024-08-03 15:22:48 -0400

    Assignment 1 Completed.

--- AndrewCodesGames-SFML-Tutorial/Entity.h
+++ AndrewCodesGames-SFML-Tutorial/Entity.h
@@ -13,6 +13,18 @@ public:
 
 	struct PhysicsData
 	{
+		PhysicsData()
+		{
+			m_vImpulse = sf::Vector2f(0.0f, 0.0f);
+		}
+
+		enum Layer
+		{
+			Enemy = 1,// 0b0001
+			Tower = 2,// 0b0010
+			Projectile = 4,// 0b0100
+		};
+
 		enum class Shape
 		{
 			Circle,
@@ -28,12 +40,70 @@ public:
 		Shape m_eShape;
 		Type m_eType;
 
+		int m_iMyLayer;
+		int m_iLayersToIgnore;
+
+		void SetLayers(int layers)
+		{
+			m_iMyLayer = layers;
+		}
+
+		void SetLayersToIgnore(int layers)
+		{
+			m_iLayersToIgnore = layers;
+		}
+
+		int GetLayersToIgnore() const
+		{
+			return m_iLayersToIgnore;
+		}
+
+		bool IsInAnyLayer(int layer) const
+		{
+			return (m_iMyLayer & layer) != 0;
+		}
+
+		void ClearCollisions()
+		{
+			m_EntitiesThatICollidedWithAlready.clear();
+		}
+
+		bool HasCollidedThisUpdate(Entity* pOtherEntity) const
+		{
+			for (Entity* pEntity : m_EntitiesThatICollidedWithAlready)
+			{
+				if (pEntity == pOtherEntity)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		void AddEntityCollision(Entity* pOtherEntity)
+		{
+			m_EntitiesThatICollidedWithAlready.push_back(pOtherEntity);
+		}
+
+		void ClearImpulse()
+		{
+			m_vImpulse = sf::Vector2f(0.0f, 0.0f);
+		}
+
+		void AddImpulse(const sf::Vector2f& impulse)
+		{
+			m_vImpulse += impulse;
+		}
+
 		float m_fRadius;
 		float m_fWidth;
 		float m_fHeight;
 
 		sf::Vector2f m_vVelocity;
+		sf::Vector2f m_vImpulse;
 		std::vector<Entity*> m_EntitiesToIgnore;
+		std::vector<Entity*> m_EntitiesThatICollidedWithAlready;
 	};
 
 	Entity(PhysicsData::Type ePhysicsType);
@@ -67,6 +137,11 @@ public:
 			}
 		}
 
+		if (entity->GetPhysicsData().IsInAnyLayer(m_PhysicsData.GetLayersToIgnore()))
+		{
+			return true;
+		}
+
 		return false;
 	}
 
@@ -95,6 +170,11 @@ public:
 		m_Sprite.setPosition(position);
 	}
 
+	void SetColor(const sf::Color& color)
+	{
+		m_Sprite.setColor(color);
+	}
+
 	void SetSprite(const sf::Sprite& sprite)
 	{
 		m_Sprite = sprite;
@@ -107,7 +187,17 @@ public:
 
 	sf::Vector2i GetClosestGridCoordinates() const
 	{
-		return sf::Vector2i(GetPosition().x/160, GetPosition().y/160);
+		return sf::Vector2i(GetPosition().x / 160, GetPosition().y / 160);
+	}
+
+	const sf::Sprite& GetSprite() const
+	{
+		return m_Sprite;
+	}
+
+	sf::Sprite& GetSpriteNonConst()
+	{
+		return m_Sprite;
 	}
 
 	void Move(const sf::Vector2f& offset)
@@ -120,7 +210,12 @@ public:
 		target.draw(m_Sprite, states);
 	}
 
-	PhysicsData GetPhysicsData() const
+	const PhysicsData& GetPhysicsData() const
+	{
+		return m_PhysicsData;
+	}
+
+	PhysicsData& GetPhysicsDataNonConst()
 	{
 		return m_PhysicsData;
 	}
@@ -135,12 +230,46 @@ public:
 		return m_iPathIndex;
 	}
 
+	void OnCollision(Entity& otherEntity);
+
+	void SetHealth(int health)
+	{
+		m_iHealth = health;
+	}
+
+	void DealDamage(int damage)
+	{
+		m_iHealth -= damage;
+		if (m_iHealth <= 0)
+		{
+			m_bDeletionRequested = true;
+		}
+	}
+
+	bool IsDeletionRequested() const
+	{
+		return m_bDeletionRequested;
+	}
+
+	void RequestDeletion()
+	{
+		m_bDeletionRequested = true;
+	}
+
 private:
 	sf::Sprite m_Sprite;
 	PhysicsData m_PhysicsData;
+	bool m_bDeletionRequested;
 
 	// Stuff that should probably be moved somewhere so that it is exclusive to enemies
 	int m_iPathIndex;
+	int m_iHealth;
+
+public:
+	float m_fAxeTimer;
+
+	// Stuff that should probably be moved somewhere so that it is exclusive to towers
+	float m_fAttackTimer;
 };
 
 #endif // ENTITY_H
--- AndrewCodesGames-SFML-Tutorial/MathHelpers.h
+++ AndrewCodesGames-SFML-Tutorial/MathHelpers.h
@@ -5,15 +5,24 @@
 
 namespace MathHelpers
 {
+	constexpr float DtoR = 0.0174533f;
+	constexpr float RtoD = 57.2958f;
+	constexpr float SQRT2 = 1.41421356237f;
+	constexpr double PI = 3.14159265358979323846;
+	constexpr double HALF_PI = PI / 2.0;
+	constexpr double QUARTER_PI = PI / 4.0;
+	constexpr double EIGHTH_PI = PI / 8.0;
+	constexpr double TWO_PI = 2.0 * PI;
+
     // Helper function to get the length of a 2d vector
-    float Length(const sf::Vector2f& rVector)
+    static float Length(const sf::Vector2f& rVector)
     {
         float fLength = sqrt(rVector.x * rVector.x + rVector.y * rVector.y);
         return fLength;
     }
 
     // Helper function to normalize a 2d vector, "normalize" means create a vector with length of 1.0f
-    sf::Vector2f Normalize(const sf::Vector2f& rVector)
+    static sf::Vector2f Normalize(const sf::Vector2f& rVector)
     {
         if (rVector.x == 0.0f && rVector.y == 0.0f)
         {
@@ -24,6 +33,32 @@ namespace MathHelpers
         sf::Vector2f vNormalizedVector(rVector.x / fLength, rVector.y / fLength);
         return vNormalizedVector;
     }
+
+	/// <summary>
+	/// Returns the clockwise angle in degrees from 0-360 relative to the down axis sf::Vector2f(0,1)
+	/// </summary>
+	/// <param name="a"></param>
+	/// <returns></returns>
+	static constexpr float Angle(const sf::Vector2f& a)
+	{
+		if (a.x == 0)
+		{
+			if (a.y > 0)
+			{
+				return 0.0f;
+			}
+			return 180.0;
+		}
+
+		float angle = atan2f(a.y, a.x) - HALF_PI;
+
+		if (angle < 0.0f)
+		{
+			angle += TWO_PI;
+		}
+
+		return angle * RtoD;
+	}
 }
 
 #endif // MATH_HELPERS_H
